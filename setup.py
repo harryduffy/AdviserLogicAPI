d70cc1536759240ab36e864db839d0b938e4be64
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-02-11 10:22:30 +1100

    merge

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='adviserlogic-api',
+   version='1.0',
+   description='A useful module',
+   author='Harry Duffy, Alexander Morton',
+   packages=['adviserlogic-api']
+)
