80c8c377a0302da1f6fd7e121e0fe6b6ae2e5eb9
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-02-11 10:51:25 +1100

    merge

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(
    name='AdviserLogicAPI',
-   packages=['AdviserLogicAPI'],
    url='https://github.com/harryduffy/AdviserLogicAPI',
    license='',
    version='1.0',
