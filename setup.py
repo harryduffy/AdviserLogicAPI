a35fc5f3fd1ffa546bd031ccdc6ddf9dbaaf84c1
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-02-11 10:46:06 +1100

    merge

--- setup.py
+++ setup.py
@@ -9,4 +9,5 @@ setup(
    version='1.0',
    description='API for Morningstar\'s AdviserLogic software.',
    author='Harry Duffy, Alexander Morton',
+   install_requires=['pandas', 'python-dotenv', 'requests', 'setuptools', 'SharePlum']
 )
