8d4615fe11ac4b1314624b494b9272262e2c1182
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-02-11 13:01:50 +1100

    merge

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
    name='AdviserLogicAPI',
-   packages=['API', 'Exceptions', 'Abstractions'],
+   packages=['AdviserLogicAPI'],
    url='https://github.com/harryduffy/AdviserLogicAPI',
    license='MIT',
    version='1.0',
