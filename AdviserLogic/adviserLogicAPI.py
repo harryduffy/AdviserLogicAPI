b66cc37af3c865b0a76f545e2a66646e614e02e4
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-02-11 13:55:24 +1100

    merge

--- AdviserLogic/adviserLogicAPI.py
+++ AdviserLogic/adviserLogicAPI.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from ..Exceptions.exceptions import AuthenticationFail, APIHealthFail, ResourceNotFoundError
+from .exceptions import AuthenticationFail, APIHealthFail, ResourceNotFoundError
 
 class AdviserLogicAPI:
 
