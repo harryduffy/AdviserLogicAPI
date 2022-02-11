54b86ac89cd5029b02015f2e673e19769845b459
harryduffy <73995105+harryduffy@users.noreply.github.com>
2022-02-11 11:18:50 +1100

    merge

--- a3/A3.py
+++ a3/A3.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from Exceptions import APIHealthFail, AuthenticationFail, ResourceNotFoundError
 
 class AdviserLogicAPI:
 
@@ -173,3 +172,15 @@ class AdviserLogicAPI:
 
         else:
             raise AuthenticationFail
+
+class APIHealthFail(Exception):
+    def __init__(self):
+        super().__init__(f"The API is not healthy.")
+
+class AuthenticationFail(Exception):
+    def __init__(self):
+        super().__init__(f"The user cannot be authenticated.")
+
+class ResourceNotFoundError(Exception):
+    def __init__(self):
+        super().__init__(f"The resource cannot be found.")
